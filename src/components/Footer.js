0f657463fc6343b9301ade34eae9d004c9f7b1c6
chenliang15405 <1165243776@qq.com>
2019-12-21 12:07:27 +0800

    feat: add for the record infomation

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,8 +7,12 @@ export default class Footer extends Component {
 
     render(){
         return (
-            <footer>
-                <p>Design by 唐宋个人主页</p>
+            <footer className='footer'>
+                <p>关于 | 学习 | 娱乐 | 成长 | Design by 唐宋个人主页</p>
+                <p>Copyright © 2019 www.cl405.top All Rights Reserved.</p>
+                <p>
+                    <a href="http://www.beian.miit.gov.cn/" target="_blank">陕ICP备19025105号</a>
+                </p>
             </footer>
         )
     }
